782cbf1afe31cdfc9f4eb113ca10968b7ff71563
ArturoOlguin <olguinmartinezarturo02@gmail.com>
2025-01-07 17:39:36 -0600

    finalizacion del documento del proyecto

--- main.py
+++ main.py
@@ -20,15 +20,12 @@ def main():
     else:
         print("No se capturaron paquetes UDP ni TCP")
 
-
     sniffer.exportar (total_de_paquetes, filename="Captura_TCP_&_UDP.pcap")
     print("Paquetes exportados a 'Captura_TCP_&_UDP.pcap'.")
     
     print(f"Total paquetes: {len(total_de_paquetes)}")
     print(f"Total paquetes TCP: {len(paquetes_tcp)}")
     print(f"Total paquetes UDP: {len(paquetes_upd)}")
-
-
-
+    
 if __name__ == "__main__":
     main()
